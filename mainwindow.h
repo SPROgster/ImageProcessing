b659007d0880f1577ef0f74ecb2834a35b63e631
SPRog <pprog@mail.ru>
2014-04-16 20:49:29 +0400

    Mask selection redone

--- mainwindow.h
+++ mainwindow.h
@@ -59,6 +59,7 @@ private:
 
     int maskValue;
     QImage* maskImage;
+    QImage* maskImageAlpha;
     QCursor* maskCursor;
 
     QImage* maskedImage;
@@ -70,6 +71,7 @@ private:
 
     QImage* selection;
     QImage* selectionBuffer;
+    QImage* selectionAlpha;
 
 private:
     QLayout* historyLayout;
