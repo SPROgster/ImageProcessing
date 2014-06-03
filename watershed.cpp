8889c58abc7e56dea85ddd34ea8090ce89e8f005
SPRog <pprog@mail.ru>
2014-06-03 21:01:59 +0400

    imageGradient complited

--- watershed.cpp
+++ watershed.cpp
@@ -4,47 +4,19 @@
 QImage* imageGradient(const QImage *origin)
 {
     // Преобразование в grayscale
-    QImage* buffer = new QImage(origin->convertToFormat(QImage::Format_RGB32));
-
-    QRgb* line;
-    int gray;
-
-    for (int i = 0; i < buffer->height(); i++)
-    {
-        line = (QRgb*)buffer->scanLine(i);
-
-        for (int j = 0; j < buffer->width(); j++)
-        {
-            gray = qGray(*line);
-
-            *(line++) = gray + (gray << 8) + (gray << 16);
-        }
-    }
-
-    /*
-    int currR, currG, currB,
-        origMin, origMax,
-        maskR, maskG, maskB;
+    QImage* buffer = new QImage(origin->width(), origin->height(), QImage::Format_RGB32);
+    buffer->fill(Qt::black);
 
     int maskSize = (buffer->width() - 2) * (buffer->height() - 2);
 
-    float min, max,
-          currRf, currGf, currBf;
+    int* gradientDenormStart;
+    int* gradientDenorm = gradientDenormStart = new int[maskSize];
 
-    float *outR, *outG, *outB,
-          *R = outR = new float[maskSize],
-          *G = outG = new float[maskSize],
-          *B = outB = new float[maskSize];
-
-    origMin = 0;
-    origMax = 255;*/
-
-    /*
     QRgb *y1, *y2, *y3,
              *yOut;
 
-    float min =  10000000;
-    float max = -10000000;
+    int min =  10000000;
+    int max = -10000000;
 
     for (int y = 1; y < buffer->height() - 1; y++)
     {
@@ -59,46 +31,27 @@ QImage* imageGradient(const QImage *origin)
 
         for (int x = 1; x < buffer->width() - 1; x++,
                                                  y1++, y2++, y3++,
-                                                 outR++, outG++, outB++)
+                                                 gradientDenorm++)
         {
-            maskR =((*(y1 - 1) & 0xFF0000) + (*(y1 + 0) & 0xFF0000) + (*(y1 + 1) & 0xFF0000) +
-                    (*(y2 - 1) & 0xFF0000)                          + (*(y2 + 1) & 0xFF0000) +
-                    (*(y3 - 1) & 0xFF0000) + (*(y3 + 0) & 0xFF0000) + (*(y3 + 1) & 0xFF0000) )
-                    >> 16;
+            // Вычисляем приближенное значение градиента
+            *gradientDenorm = abs(-(hsvValue(*(y1 - 1)) + 2 * hsvValue(*y1) + hsvValue(*(y1 + 1)))
+                                  + hsvValue(*(y3 - 1)) + 2 * hsvValue(*y3) + hsvValue(*(y3 + 1)))
 
-            currR = (*y2 & 0xFF0000) >> 16;
-            currG = (*y2 & 0x00FF00) >> 8;
-            currB =  *y2 & 0x0000FF;
+                            + abs(-(hsvValue(*(y1 - 1)) + 2 * hsvValue(*(y2 - 1)) + hsvValue(*(y3 - 1)))
+                                  + hsvValue(*(y1 + 1)) + 2 * hsvValue(*(y2 + 1)) + hsvValue(*(y3 + 1)));
 
-            if (currR > origMax) origMax = currR;
-            if (currG > origMax) origMax = currG;
-            if (currB > origMax) origMax = currB;
-
-            if (currR < origMin) origMin = currR;
-            if (currG < origMin) origMin = currG;
-            if (currB < origMin) origMin = currB;
-
-            currRf = (A + 8.) * (float)currR - (float)maskR;
-            currGf = (A + 8.) * (float)currG - (float)maskG;
-            currBf = (A + 8.) * (float)currB - (float)maskB;
-
-            if (currRf > max) max = currRf;
-            if (currGf > max) max = currGf;
-            if (currBf > max) max = currBf;
-
-            if (currRf < min) min = currRf;
-            if (currGf < min) min = currGf;
-            if (currBf < min) min = currBf;
-
-            *outR = currRf;
-            *outG = currGf;
-            *outB = currBf;
+            // Ищим максимум/минимум, чтобы потом отмасштабировать и уложить в изображение
+            if (*gradientDenorm > max) max = *gradientDenorm;
+            if (*gradientDenorm < min) min = *gradientDenorm;
         }
     }
 
-    outR = R; outG = G; outB = B;
+    // Переходим на начало градиента
+    gradientDenorm = gradientDenormStart;
 
-    float mulCoef = (origMax - origMin) / (max - min);
+    float mulCoef = 255. / (max - min);
+
+    QRgb pixel;
 
     for (int y = 1; y < buffer->height() - 1; y++)
     {
@@ -108,32 +61,27 @@ QImage* imageGradient(const QImage *origin)
 
         for (int x = 1; x < buffer->width() - 1; x++,
                                                  yOut++,
-                                                 outR++, outG++, outB++)
+                                                 gradientDenorm++)
         {
-            currR = (int)( (*outR - min) * mulCoef) + origMin;
-            currG = (int)( (*outG - min) * mulCoef) + origMin;
-            currB = (int)( (*outB - min) * mulCoef) + origMin;
+            pixel = (int)( (*gradientDenorm - min) * mulCoef);
 
-            *yOut = (currR << 16) + (currG << 8) + currB;
+            *yOut = (pixel << 16) + (pixel << 8) + pixel;
         }
     }
 
-    if (selection == 0)
-    {
-        ui->imageView->setPixmap(QPixmap::fromImage(*buffer));
-        repaint();
-    }
-    else
-    {
-        if (selectionBuffer != 0)
-            delete selectionBuffer;
-        selectionBuffer = new QImage(*buffer);
+    delete [] gradientDenormStart;
 
-        selectionPreview();
-
-    }
-
-    delete [] R; delete [] G; delete [] B;
-    delete buffer;*/
     return buffer;
+}
+
+
+int hsvValue(QRgb color)
+{
+    int R = (color >> 16) & 0xFF,
+        G = (color >>  8) & 0xFF,
+        B = (color      ) & 0xFF;
+
+    int max = (R > G) ? R : G;
+
+    return (max > B) ? max : B;
 }
--- watershed.h
+++ watershed.h
@@ -1,6 +1,7 @@
 #ifndef WATERSHED_H
 #define WATERSHED_H
 
+int hsvValue(QRgb color);
 QImage* imageGradient(const QImage* origin);
 
 #endif // WATERSHED_H
