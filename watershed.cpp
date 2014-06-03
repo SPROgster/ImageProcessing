de5addde4d56a426a0e6d81bb2b00dec4466fa28
SPRog <pprog@mail.ru>
2014-06-03 22:00:00 +0400

    In imageGradient grayscale converion added

--- watershed.cpp
+++ watershed.cpp
@@ -3,11 +3,25 @@
 
 QImage* imageGradient(const QImage *origin)
 {
-    QImage* buffer = new QImage(origin->convertToFormat(QImage::Format_RGB32, Qt::MonoOnly));
+    // Преобразование в grayscale
+    QImage* buffer = new QImage(origin->convertToFormat(QImage::Format_RGB32));
 
-    /*QRgb *y1, *y2, *y3,
-         *yOut;
+    QRgb* line;
+    int gray;
 
+    for (int i = 0; i < buffer->height(); i++)
+    {
+        line = (QRgb*)buffer->scanLine(i);
+
+        for (int j = 0; j < buffer->width(); j++)
+        {
+            gray = qGray(*line);
+
+            *(line++) = gray + (gray << 8) + (gray << 16);
+        }
+    }
+
+    /*
     int currR, currG, currB,
         origMin, origMax,
         maskR, maskG, maskB;
@@ -23,131 +37,62 @@ QImage* imageGradient(const QImage *origin)
           *B = outB = new float[maskSize];
 
     origMin = 0;
-    origMax = 255;
+    origMax = 255;*/
 
-    min =  10000000;
-    max = -10000000;
+    /*
+    QRgb *y1, *y2, *y3,
+             *yOut;
 
-    if (fullSquare)
+    float min =  10000000;
+    float max = -10000000;
+
+    for (int y = 1; y < buffer->height() - 1; y++)
     {
-        for (int y = 1; y < buffer->height() - 1; y++)
+
+        y1 = (QRgb*)origin->scanLine(y - 1);
+        y2 = (QRgb*)origin->scanLine(y    );
+        y3 = (QRgb*)origin->scanLine(y + 1);
+
+        y1++;
+        y2++;
+        y3++;
+
+        for (int x = 1; x < buffer->width() - 1; x++,
+                                                 y1++, y2++, y3++,
+                                                 outR++, outG++, outB++)
         {
+            maskR =((*(y1 - 1) & 0xFF0000) + (*(y1 + 0) & 0xFF0000) + (*(y1 + 1) & 0xFF0000) +
+                    (*(y2 - 1) & 0xFF0000)                          + (*(y2 + 1) & 0xFF0000) +
+                    (*(y3 - 1) & 0xFF0000) + (*(y3 + 0) & 0xFF0000) + (*(y3 + 1) & 0xFF0000) )
+                    >> 16;
 
-            y1 = (QRgb*)origin->scanLine(y - 1);
-            y2 = (QRgb*)origin->scanLine(y    );
-            y3 = (QRgb*)origin->scanLine(y + 1);
+            currR = (*y2 & 0xFF0000) >> 16;
+            currG = (*y2 & 0x00FF00) >> 8;
+            currB =  *y2 & 0x0000FF;
 
-            y1++;
-            y2++;
-            y3++;
+            if (currR > origMax) origMax = currR;
+            if (currG > origMax) origMax = currG;
+            if (currB > origMax) origMax = currB;
 
-            for (int x = 1; x < buffer->width() - 1; x++,
-                                                     y1++, y2++, y3++,
-                                                     outR++, outG++, outB++)
-            {
-                maskR =((*(y1 - 1) & 0xFF0000) + (*(y1 + 0) & 0xFF0000) + (*(y1 + 1) & 0xFF0000) +
-                        (*(y2 - 1) & 0xFF0000)                          + (*(y2 + 1) & 0xFF0000) +
-                        (*(y3 - 1) & 0xFF0000) + (*(y3 + 0) & 0xFF0000) + (*(y3 + 1) & 0xFF0000) )
-                        >> 16;
+            if (currR < origMin) origMin = currR;
+            if (currG < origMin) origMin = currG;
+            if (currB < origMin) origMin = currB;
 
-                maskG =((*(y1 - 1) & 0x00FF00) + (*(y1 + 0) & 0x00FF00) + (*(y1 + 1) & 0x00FF00) +
-                        (*(y2 - 1) & 0x00FF00)                          + (*(y2 + 1) & 0x00FF00) +
-                        (*(y3 - 1) & 0x00FF00) + (*(y3 + 0) & 0x00FF00) + (*(y3 + 1) & 0x00FF00))
-                        >> 8;
+            currRf = (A + 8.) * (float)currR - (float)maskR;
+            currGf = (A + 8.) * (float)currG - (float)maskG;
+            currBf = (A + 8.) * (float)currB - (float)maskB;
 
-                maskB = (*(y1 - 1) & 0x0000FF) + (*(y1 + 0) & 0x0000FF) + (*(y1 + 1) & 0x0000FF) +
-                        (*(y2 - 1) & 0x0000FF)                          + (*(y2 + 1) & 0x0000FF) +
-                        (*(y3 - 1) & 0x0000FF) + (*(y3 + 0) & 0x0000FF) + (*(y3 + 1) & 0x0000FF);
+            if (currRf > max) max = currRf;
+            if (currGf > max) max = currGf;
+            if (currBf > max) max = currBf;
 
-                currR = (*y2 & 0xFF0000) >> 16;
-                currG = (*y2 & 0x00FF00) >> 8;
-                currB =  *y2 & 0x0000FF;
+            if (currRf < min) min = currRf;
+            if (currGf < min) min = currGf;
+            if (currBf < min) min = currBf;
 
-                if (currR > origMax) origMax = currR;
-                if (currG > origMax) origMax = currG;
-                if (currB > origMax) origMax = currB;
-
-                if (currR < origMin) origMin = currR;
-                if (currG < origMin) origMin = currG;
-                if (currB < origMin) origMin = currB;
-
-                currRf = (A + 8.) * (float)currR - (float)maskR;
-                currGf = (A + 8.) * (float)currG - (float)maskG;
-                currBf = (A + 8.) * (float)currB - (float)maskB;
-
-                if (currRf > max) max = currRf;
-                if (currGf > max) max = currGf;
-                if (currBf > max) max = currBf;
-
-                if (currRf < min) min = currRf;
-                if (currGf < min) min = currGf;
-                if (currBf < min) min = currBf;
-
-                *outR = currRf;
-                *outG = currGf;
-                *outB = currBf;
-            }
-        }
-    }
-    else
-    {
-        for (int y = 1; y < buffer->height() - 1; y++)
-        {
-
-            y1 = (QRgb*)origin->scanLine(y - 1);
-            y2 = (QRgb*)origin->scanLine(y    );
-            y3 = (QRgb*)origin->scanLine(y + 1);
-
-            y1++;
-            y2++;
-            y3++;
-
-            for (int x = 1; x < buffer->width() - 1; x++,
-                                                     y1++, y2++, y3++,
-                                                     outR++, outG++, outB++)
-            {
-                maskR =(                         (*(y1 + 0) & 0xFF0000)                          +
-                        (*(y2 - 1) & 0xFF0000)                          + (*(y2 + 1) & 0xFF0000) +
-                                                 (*(y3 + 0) & 0xFF0000)                          )
-                        >> 16;
-
-                maskG =(                         (*(y1 + 0) & 0x00FF00)                          +
-                        (*(y2 - 1) & 0x00FF00)                          + (*(y2 + 1) & 0x00FF00) +
-                                                 (*(y3 + 0) & 0x00FF00)                         )
-                        >> 8;
-
-                maskB =                          (*(y1 + 0) & 0x0000FF)                          +
-                        (*(y2 - 1) & 0x0000FF)                          + (*(y2 + 1) & 0x0000FF) +
-                                                 (*(y3 + 0) & 0x0000FF)                         ;
-
-                currR = (*y2 & 0xFF0000) >> 16;
-                currG = (*y2 & 0x00FF00) >> 8;
-                currB =  *y2 & 0x0000FF;
-
-                if (currR > origMax) origMax = currR;
-                if (currG > origMax) origMax = currG;
-                if (currB > origMax) origMax = currB;
-
-                if (currR < origMin) origMin = currR;
-                if (currG < origMin) origMin = currG;
-                if (currB < origMin) origMin = currB;
-
-                currRf = (A + 4.) * (float)currR - (float)maskR;
-                currGf = (A + 4.) * (float)currG - (float)maskG;
-                currBf = (A + 4.) * (float)currB - (float)maskB;
-
-                if (currRf > max) max = currRf;
-                if (currGf > max) max = currGf;
-                if (currBf > max) max = currBf;
-
-                if (currRf < min) min = currRf;
-                if (currGf < min) min = currGf;
-                if (currBf < min) min = currBf;
-
-                *outR = currRf;
-                *outG = currGf;
-                *outB = currBf;
-            }
+            *outR = currRf;
+            *outG = currGf;
+            *outB = currBf;
         }
     }
 
