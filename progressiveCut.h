a8759dff1eb7b68b3194c2855cbd6a130bfba4a3
SPRog <pprog@mail.ru>
2014-06-06 20:42:54 +0400

    inline functions moved to header

--- progressiveCut.h
+++ progressiveCut.h
@@ -13,4 +13,20 @@ struct Graph
 float Bpq(const QColor a, const int xa, const int ya, const QColor b, const int xb, const int yb, const float delta);
 Graph *createGraph(const QImage& image);
 
+inline float
+distance(int x1, int y1, int x2, int y2)
+{
+    return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}
+
+inline float
+norma2(const QColor a, const QColor b)
+{
+    int red  = a.red()   - b.red();
+    int green= a.green() - b.green();
+    int blue = a.blue()  - b.blue();
+
+    return (red * red + green * green + blue * blue);
+}
+
 #endif // PROGRESSIVECUT_H
